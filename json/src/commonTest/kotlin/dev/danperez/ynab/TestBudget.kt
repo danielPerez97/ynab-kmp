5230d4ab2f4a8e0a2915fea9dc7465d719310a0d
Daniel Perez <doperez1997@gmail.com>
2023-03-26 21:58:40 -0500

    Corrected a test using the wrong file name

--- json/src/commonTest/kotlin/dev/danperez/ynab/TestBudget.kt
+++ json/src/commonTest/kotlin/dev/danperez/ynab/TestBudget.kt
@@ -4,6 +4,7 @@ import dev.danperez.ynab.budget.Budget
 import kotlinx.serialization.ExperimentalSerializationApi
 import kotlinx.serialization.json.okio.decodeFromBufferedSource
 import kotlin.test.Test
+import kotlin.test.assertEquals
 
 @OptIn(ExperimentalSerializationApi::class)
 class TestBudget: BaseJsonTest()
@@ -12,7 +13,9 @@ class TestBudget: BaseJsonTest()
     @Test
     fun testBudgetSerializes()
     {
-        val source = "budget.json".readBufferedSource()
-        json.decodeFromBufferedSource<Budget>(source)
+        val source = "Budget.json".readBufferedSource()
+        val budget = json.decodeFromBufferedSource<Budget>(source)
+
+        assertEquals("My Budget", budget.name)
     }
 }
