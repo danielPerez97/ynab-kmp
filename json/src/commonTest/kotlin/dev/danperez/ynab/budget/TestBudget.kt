8f8a48b6e0af243c612d65f469459310c8631652
Daniel Perez <doperez1997@gmail.com>
2023-03-31 10:45:29 -0500

    Added a test for Budget.kt wrapped in a response

--- json/src/commonTest/kotlin/dev/danperez/ynab/budget/TestBudget.kt
+++ json/src/commonTest/kotlin/dev/danperez/ynab/budget/TestBudget.kt
@@ -1,6 +1,7 @@
 package dev.danperez.ynab.budget
 
 import dev.danperez.ynab.BaseJsonTest
+import dev.danperez.ynab.Response
 import dev.danperez.ynab.readBufferedSource
 import kotlinx.serialization.ExperimentalSerializationApi
 import kotlinx.serialization.json.okio.decodeFromBufferedSource
@@ -18,5 +19,15 @@ class TestBudget: BaseJsonTest()
         val budget = json.decodeFromBufferedSource<Budget>(source)
 
         assertEquals("My Budget", budget.name)
+    }
+
+    @Test
+    fun testBudgetResponseSerializes()
+    {
+        val source = "budget/BudgetResponse.json".readBufferedSource()
+        val userMessage = json.decodeFromBufferedSource<Response<Budget>>(source)
+
+        require(userMessage is Response.Ok<Budget>)
+        assertEquals("My Budget", userMessage.data.name)
     }
 }
