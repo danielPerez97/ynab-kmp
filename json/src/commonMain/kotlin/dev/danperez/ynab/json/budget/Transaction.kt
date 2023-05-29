79f5cfcb3796f8cb81891bd3c10ec2f5e46ad588
Daniel Perez <doperez1997@gmail.com>
2023-05-29 15:49:17 -0500

    Updated some Transaction.kt fields to be nullable + modified their JSON test to reflect

--- json/src/commonMain/kotlin/dev/danperez/ynab/json/budget/Transaction.kt
+++ json/src/commonMain/kotlin/dev/danperez/ynab/json/budget/Transaction.kt
@@ -10,20 +10,20 @@ sealed interface Transaction
     val id: String
     val date: String
     val amount: Int
-    val memo: String
+    val memo: String?
     val cleared: String
     val approved: Boolean
-    val flagColor: String
+    val flagColor: String?
     val accountId: String
-    val payeeId: String
-    val categoryId: String
-    val transferAccountId: String
-    val transferTransactionId: String
-    val matchedTransactionId: String
-    val importId: String
-    val importPayeeName: String
-    val importPayeeNameOriginal: String
-    val debtTransactionType: String
+    val payeeId: String?
+    val categoryId: String?
+    val transferAccountId: String?
+    val transferTransactionId: String?
+    val matchedTransactionId: String?
+    val importId: String?
+    val importPayeeName: String?
+    val importPayeeNameOriginal: String?
+    val debtTransactionType: String?
     val deleted: Boolean
 
     @Serializable
@@ -31,20 +31,20 @@ sealed interface Transaction
         override val id: String,
         override val date: String,
         override val amount: Int,
-        override val memo: String,
+        override val memo: String?,
         override val cleared: String,
         override val approved: Boolean,
-        @SerialName("flag_color") override val flagColor: String,
+        @SerialName("flag_color") override val flagColor: String?,
         @SerialName("account_id") override val accountId: String,
-        @SerialName("payee_id") override val payeeId: String,
-        @SerialName("category_id") override val categoryId: String,
-        @SerialName("transfer_account_id") override val transferAccountId: String,
-        @SerialName("transfer_transaction_id") override val transferTransactionId: String,
-        @SerialName("matched_transaction_id") override val matchedTransactionId: String,
-        @SerialName("import_id") override val importId: String,
-        @SerialName("import_payee_name") override val importPayeeName: String,
-        @SerialName("import_payee_name_original") override val importPayeeNameOriginal: String,
-        @SerialName("debt_transaction_type") override val debtTransactionType: String,
+        @SerialName("payee_id") override val payeeId: String?,
+        @SerialName("category_id") override val categoryId: String?,
+        @SerialName("transfer_account_id") override val transferAccountId: String?,
+        @SerialName("transfer_transaction_id") override val transferTransactionId: String?,
+        @SerialName("matched_transaction_id") override val matchedTransactionId: String?,
+        @SerialName("import_id") override val importId: String?,
+        @SerialName("import_payee_name") override val importPayeeName: String?,
+        @SerialName("import_payee_name_original") override val importPayeeNameOriginal: String?,
+        @SerialName("debt_transaction_type") override val debtTransactionType: String?,
         override val deleted: Boolean,
     ): Transaction
 
@@ -53,20 +53,20 @@ sealed interface Transaction
         override val id: String,
         override val date: String,
         override val amount: Int,
-        override val memo: String,
+        override val memo: String?,
         override val cleared: String,
         override val approved: Boolean,
-        @SerialName("flag_color") override val flagColor: String,
+        @SerialName("flag_color") override val flagColor: String?,
         @SerialName("account_id") override val accountId: String,
-        @SerialName("payee_id") override val payeeId: String,
-        @SerialName("category_id") override val categoryId: String,
-        @SerialName("transfer_account_id") override val transferAccountId: String,
-        @SerialName("transfer_transaction_id") override val transferTransactionId: String,
-        @SerialName("matched_transaction_id") override val matchedTransactionId: String,
-        @SerialName("import_id") override val importId: String,
-        @SerialName("import_payee_name") override val importPayeeName: String,
-        @SerialName("import_payee_name_original") override val importPayeeNameOriginal: String,
-        @SerialName("debt_transaction_type") override val debtTransactionType: String,
+        @SerialName("payee_id") override val payeeId: String?,
+        @SerialName("category_id") override val categoryId: String?,
+        @SerialName("transfer_account_id") override val transferAccountId: String?,
+        @SerialName("transfer_transaction_id") override val transferTransactionId: String?,
+        @SerialName("matched_transaction_id") override val matchedTransactionId: String?,
+        @SerialName("import_id") override val importId: String?,
+        @SerialName("import_payee_name") override val importPayeeName: String?,
+        @SerialName("import_payee_name_original") override val importPayeeNameOriginal: String?,
+        @SerialName("debt_transaction_type") override val debtTransactionType: String?,
         override val deleted: Boolean,
         @SerialName("account_name") val accountName: String,
         @SerialName("payee_name") val payeeName: String,
