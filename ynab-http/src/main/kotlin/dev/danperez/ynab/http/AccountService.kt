bb09543a7664a217a221484123d8b003f6db5da1
Daniel Perez <doperez1997@gmail.com>
2023-05-30 11:24:45 -0500

    Added Javadocs to AccountService.kt

--- ynab-http/src/main/kotlin/dev/danperez/ynab/http/AccountService.kt
+++ ynab-http/src/main/kotlin/dev/danperez/ynab/http/AccountService.kt
@@ -10,12 +10,26 @@ import retrofit2.http.Path
 
 interface AccountService
 {
+    /**
+     * @param budgetId The id of the budget. “last-used” can be used to specify the last used budget and “default” can
+     * be used if default budget selection is enabled (see: https://api.ynab.com/#oauth-default-budget).
+     */
     @GET("/budget/{budget_id}/accounts")
     suspend fun getAccounts(@Path("budget_id") budgetId: String): Response<List<Account>>
 
+    /**
+     * @param budgetId The id of the budget. “last-used” can be used to specify the last used budget and “default” can
+     * be used if default budget selection is enabled (see: https://api.ynab.com/#oauth-default-budget).
+     * @param newAccount The account to create.
+     */
     @POST("/budget/{budget_id}/accounts")
     suspend fun createAccount(@Path("budget_id") budgetId: String, @Body newAccount: NewAccount): Response<Account>
 
+    /**
+     * @param budgetId The id of the budget. “last-used” can be used to specify the last used budget and “default” can
+     * be used if default budget selection is enabled (see: https://api.ynab.com/#oauth-default-budget).
+     * @param accountId The id of the account.
+     */
     @GET("/budget/{budget_id}/accounts/{account_id}")
     suspend fun getAccountById(@Path("budget_id") budgetId: String, @Path("account_id") accountId: String): Response<Account>
 }
