c8cce05a3e744b20a3ab7e258571ce2eb84875dc
Daniel Perez <doperez1997@gmail.com>
2023-06-14 10:52:12 -0500

    Updated ResponseSerializer.kt + Response.kt

--- json/src/commonMain/kotlin/dev/danperez/ynab/json/Response.kt
+++ json/src/commonMain/kotlin/dev/danperez/ynab/json/Response.kt
@@ -6,10 +6,13 @@ import kotlinx.serialization.Serializable
 @Serializable(with = ResponseSerializer::class)
 sealed class Response<out T>
 {
+    @Serializable
     data class Ok<out T>(
         val data: T,
         val serverKnowledge: Int? = null,
     ): Response<T>()
+
+    @Serializable
     data class Error(
         val id: String,
         val name: String,
--- json/src/commonMain/kotlin/dev/danperez/ynab/json/internal/ResponseSerializer.kt
+++ json/src/commonMain/kotlin/dev/danperez/ynab/json/internal/ResponseSerializer.kt
@@ -18,6 +18,33 @@ import kotlinx.serialization.json.contentOrNull
 import kotlinx.serialization.json.jsonObject
 import kotlinx.serialization.json.jsonPrimitive
 
+/**
+ * The [ResponseSerializer] is a convenience serializer to skip serializing the wrapper "data" object in the json. For
+ * example, serializing the following JSON would require a class definition akin to the following on the "data" key.
+ *
+ * Kotlin:
+ * data class MonthResponse(val month: Month)
+ *
+ * JSON:
+ * {
+ *   "data": {
+ *     "month": {
+ *       "month": "April",
+ *       "note": "string",
+ *       "income": 0,
+ *       "budgeted": 0,
+ *       "activity": 0,
+ *       "to_be_budgeted": 0,
+ *       "age_of_money": 0,
+ *       "deleted": true,
+ *       "categories": []
+ *     }
+ *   }
+ * }
+ *
+ * The ResponseSerializer will let you skip the data key so you don't have to return a MonthResponse, as long as there
+ * is ONLY ONE key other than server_knowledge, which the serializer is smart enough to account for.
+ */
 @OptIn(ExperimentalSerializationApi::class)
 internal class ResponseSerializer<T>(private val dataSerializer: KSerializer<T>): KSerializer<Response<T>> {
     @OptIn(InternalSerializationApi::class)
@@ -45,13 +72,30 @@ internal class ResponseSerializer<T>(private val dataSerializer: KSerializer<T>)
                 detail = errorElement["detail"]!!.jsonPrimitive.content,
             )
         }
-        val nextObject: JsonObject = element["data"]!!.jsonObject
-        val nextElement: JsonElement = nextObject[nextObject.keys.first()]!!
-        val serverKnowledge = element["server_knowledge"]?.jsonPrimitive
-        return Response.Ok(
-            data = decoder.json.decodeFromJsonElement(dataSerializer, nextElement),
-            serverKnowledge = serverKnowledge?.contentOrNull?.toInt()
-        )
+        val dataElement: JsonObject = element["data"]!!.jsonObject
+        val serverKnowledge = dataElement["server_knowledge"]?.jsonPrimitive
+        val keySizeWithoutServerKnowledge = dataElement.keys.size.let { if(serverKnowledge != null) it - 1 else it }
+        println("Key Size: $keySizeWithoutServerKnowledge")
+
+        return if(keySizeWithoutServerKnowledge == 1) {
+            val nextElement = dataElement[dataElement.keys.first()]!!
+            Response.Ok(
+                data = decoder.json.decodeFromJsonElement(dataSerializer, nextElement),
+                serverKnowledge = serverKnowledge?.contentOrNull?.toInt()
+            )
+        } else {
+            Response.Ok(
+                data = decoder.json.decodeFromJsonElement(dataSerializer, dataElement),
+                serverKnowledge = serverKnowledge?.contentOrNull?.toInt(),
+            )
+        }
+//        val nextElement: JsonElement = nextObject[nextObject.keys.first()]!!
+//        val keys = (nextElement as JsonObject).keys.size
+//        println("Keys Size: $keys")
+//        return Response.Ok(
+//            data = decoder.json.decodeFromJsonElement(dataSerializer, nextElement),
+//            serverKnowledge = serverKnowledge?.contentOrNull?.toInt()
+//        )
     }
 
     override fun serialize(encoder: Encoder, value: Response<T>) {
